77ed5f207aeecb7ea469ff9dda983ac5e07aa60a
yesyes295 <yesyes295@yahoo.com>
2016-07-01 22:21:55 +0700

    chỉnh lại giao diện (chưa xong), code logic form Quản lý sảnh

--- QuanLyTiecCuoiDTO/DTO_Sanh.cs
+++ QuanLyTiecCuoiDTO/DTO_Sanh.cs
@@ -9,16 +9,16 @@ namespace DTO
     public class DTO_Sanh
     {
         public DTO_Sanh() { }
-        public DTO_Sanh(string tenSanh, char maLoaiSanh, int soLuongBanToiDa, string ghiChu)
+        public DTO_Sanh(string tenSanh, int maLoaiSanh, int soLuongBanToiDa, string ghiChu)
         {
             this.tenSanh = tenSanh;
             this.maLoaiSanh = maLoaiSanh;
             this.soLuongBanToiDa = soLuongBanToiDa;
             this.ghiChu = ghiChu;
         }
-        public string maSanh;
+        public int maSanh;
         public string tenSanh;
-        public char maLoaiSanh;
+        public int maLoaiSanh;
         public int soLuongBanToiDa;
         public string ghiChu;
     }
