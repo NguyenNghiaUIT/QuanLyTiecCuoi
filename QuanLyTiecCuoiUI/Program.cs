faa575132c254bf8b52af5d62d66f9aff7dd225d
Healer <tamphu.pn@gmail.com>
2016-07-03 15:56:51 +0700

    phú 03/07

--- QuanLyTiecCuoiUI/Program.cs
+++ QuanLyTiecCuoiUI/Program.cs
@@ -1,42 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using DAO;
-using QuanLyTiecCuoiUI.FormFeature.ConfigDatabase;
-
-namespace QuanLyTiecCuoiUI
-{
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-         
-            DatabaseHelper.CONNECTION_STRING = QuanLyTiecCuoiUI.Properties.Settings.Default.ConnectionString;
-            Console.WriteLine(DatabaseHelper.CONNECTION_STRING);
-            bool openSuscess = DatabaseHelper.OpenConnection();
-            DatabaseHelper.CloseConnection();
-            if(!openSuscess){
-                 Application.Run(new frmConfigDatabase());
-            }else{
-                Application.Run(new frmNhanDatTiecCuoi());
-                //Application.Run(new frmManHinhChinh());
-                //Application.Run(new frmQuanLyMonAn());
-            } 
-        }
-    }
-}
-=======
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -73,4 +35,3 @@ namespace QuanLyTiecCuoiUI
         }
     }
 }
->>>>>>> 5feb8d9e1ccfed141fd2bbf6ffb2e1872b8cea78
