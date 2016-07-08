58c293448f2465f285786daf52030965b3e78cd8
Healer <tamphu.pn@gmail.com>
2016-07-08 20:43:32 +0700

    thêm DTO

---
+++ QuanLyTiecCuoiDAO/DAO_QuanLyNhanVien.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using System.Data;
+
+namespace DAO
+{
+    public class DAO_QuanLyNhanVien
+    {
+        public static DataTable LoadChucVu()
+        {
+            string sTruyVan = "Select * from CHUCVU";
+            return DatabaseHelper.GetData(sTruyVan);
+        }
+        public static DataTable LoadDanhSachNhanVien()
+        {
+            string sTruyVan = "Select * from NHANVIEN, CHUCVU where NHANVIEN.MaChucVu = CHUCVU.MaChucVu";
+            return DatabaseHelper.GetData(sTruyVan);
+        }
+    }
+}
