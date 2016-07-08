4387317dd065a59e113aa2c89e1b4815663ae4bd
Healer <tamphu.pn@gmail.com>
2016-07-08 20:44:36 +0700

    BUS

---
+++ QuanLyTiecCuoiBUS/BUS_QuanLyNhanVien.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using System.Data;
+using DAO;
+namespace BUS
+{
+    public class BUS_QuanLyNhanVien
+    {
+        public static DataTable LoadChucVu()
+        {
+            return DAO_QuanLyNhanVien.LoadChucVu();
+        }
+        public static DataTable LoadDanhSachNhanVien()
+        {
+            return DAO_QuanLyNhanVien.LoadDanhSachNhanVien();
+        }
+    }
+}
